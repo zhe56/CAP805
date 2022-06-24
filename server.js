fa9b8a25fb4d1d82cf21e835d7d56f5ac41a79d3
Zhenhui He <zhe56@myseneca.ca>
2022-06-24 14:03:47 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const exphbs = require("express-handlebars");
+var express = require("express");
+var app = express();
+app.set("view engine", ".hbs");
+var HTTP_PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    helpers: {
+      navLink: (url, options) => {
+        return (
+          "<li" +
+          (url == app.locals.activeRoute
+            ? ' class="nav-item active" '
+            : ' class="nav-item" ') +
+          '><a class="nav-link" href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          "</a></li>"
+        );
+      },
+
+      equal: (lvalue, rvalue, options) => {
+        if (arguments.length < 3)
+          throw new Error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
+
+//to fix the highlighting of the nav
+app.use((req, res, next) => {
+  let route = req.path.substring(1);
+  app.locals.activeRoute =
+    "/" +
+    (isNaN(route.split("/")[1])
+      ? route.replace(/\/(?!.*)/, "")
+      : route.replace(/\/(.*)/, ""));
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log("server listening on port: " + HTTP_PORT);
+});
